e07c519cdb05a5e9b53aa4a8111247f740132597
Fazal00007 <111645285+Fazal00007@users.noreply.github.com>
2023-04-01 10:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://toaa:toaa69@cluster0.eduoooo.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Immortalxscanner:n41PhKXrohZxOpSj@cluster0.fvthilg.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Immortal")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
